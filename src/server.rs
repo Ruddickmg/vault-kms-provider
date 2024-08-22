099ec342cc1e487cf508f4f96341e8dadc306fe9
grant <ruddickmg@gmail.com>
2024-08-21 22:05:48 -1000

    feat: print mounted directory

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,13 @@
 extern crate lib;
 
+use std::fs;
 use lib::{
     configuration, kms::key_management_service_server::KeyManagementServiceServer,
     utilities::socket::create_unix_socket, vault,
 };
 use tokio::join;
 use tonic::transport::Server;
+use lib::utilities::token;
 
 mod checks;
 
@@ -15,6 +17,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let socket_config = configuration::socket();
     let vault_kms_server =
         vault::VaultKmsServer::new(&vault_config.vault_transit_key, &vault_config.vault_address);
+    let token = token::auth_token();
+    println!("Auth token: {:?}", token);
+    let paths = fs::read_dir("/var/run/secrets/kubernetes.io/serviceaccount").unwrap();
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display())
+    }
     println!(
         "Server listening to socket @\"{}\", connecting to vault @\"{}\"",
         socket_config.socket_path, vault_config.vault_address
