6400b3f8de48247d790773058aa35884fb30a04b
grant <ruddickmg@gmail.com>
2024-08-23 18:53:24 -1000

    feat: mount token for vault, add debug logs for token

--- src/utilities/token.rs
+++ src/utilities/token.rs
@@ -1,7 +1,7 @@
 use crate::configuration::vault::VaultConfiguration;
 use std::io::ErrorKind;
 use std::{fs, io};
-use tracing::instrument;
+use tracing::{debug, instrument};
 
 #[instrument]
 pub fn auth_token() -> io::Result<String> {
@@ -9,9 +9,12 @@ pub fn auth_token() -> io::Result<String> {
     let token = config.vault_token;
     let path = config.vault_token_path;
     if token != "" {
+        debug!("Using input token of length: {}", token.len());
         Ok(token.to_string())
     } else if path != "" {
-        fs::read_to_string(path)
+        let token = fs::read_to_string(path)?;
+        debug!("Using mounted token of length: {}", token.len());
+        Ok(token)
     } else {
         Err(std::io::Error::new(ErrorKind::Other, "No auth token found"))
     }
