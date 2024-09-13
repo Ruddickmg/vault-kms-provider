5f2bd2ff4e9e2aeb54a519f421840e0d4acfc2cb
grant <ruddickmg@gmail.com>
2024-09-13 06:44:22 -1000

    fix: add tests for env utility

--- src/configuration/vault.rs
+++ src/configuration/vault.rs
@@ -1,4 +1,4 @@
-use crate::utilities::env::get_env;
+use crate::utilities::env::{get_env, get_env_option};
 
 const DEFAULT_VAULT_ADDRESS: &str = "https://vault.vault.cluster.svc.local:8200";
 const DEFAULT_VAULT_TRANSIT_KEY: &str = "vault-kms-provider";
@@ -16,9 +16,8 @@ impl VaultConfiguration {
     pub fn new() -> Self {
         Self {
             vault_role: get_env("VAULT_ROLE", DEFAULT_VAULT_ROLE),
-            vault_token: std::env::var("VAULT_TOKEN").map_or(None, |token| Some(token.to_string())),
-            vault_token_path: std::env::var("VAULT_TOKEN_PATH")
-                .map_or(None, |token| Some(token.to_string())),
+            vault_token: get_env_option("VAULT_TOKEN"),
+            vault_token_path: get_env_option("VAULT_TOKEN_PATH"),
             vault_address: get_env("VAULT_ADDRESS", DEFAULT_VAULT_ADDRESS),
             vault_transit_key: get_env("VAULT_TRANSIT_KEY", DEFAULT_VAULT_TRANSIT_KEY),
         }
