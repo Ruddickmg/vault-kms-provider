82bdab54ccaae5f74eedcd049f39ea0e536f897d
Marcus Ruddick <Ruddickmg@gmail.com>
2024-09-28 11:09:27 -1000

    fix: disable external crate logs
    * feat: whitelist log targets
    
    * feat: show detailed logs for debug or above level
    
    * feat: update version
    
    * feat: update dependencies

--- src/utilities/logging.rs
+++ src/utilities/logging.rs
@@ -1,9 +1,18 @@
 use crate::configuration::logging::LoggingConfiguration;
 use tracing::debug;
+use tracing_subscriber::EnvFilter;
 
 pub fn initialize() {
     let config = LoggingConfiguration::new();
-    let subscriber = tracing_subscriber::FmtSubscriber::builder().with_max_level(config.level);
+    let directive = if config.level < tracing::Level::DEBUG {
+        [&env!("CARGO_PKG_NAME").replace("-", "_"), "lib", "server"]
+            .map(|target| format!("{}={}", target, config.level))
+            .join(",")
+    } else {
+        config.level.to_string()
+    };
+    let subscriber =
+        tracing_subscriber::FmtSubscriber::builder().with_env_filter(EnvFilter::new(&directive));
 
     match config.format.as_str() {
         "json" => subscriber.json().init(),
