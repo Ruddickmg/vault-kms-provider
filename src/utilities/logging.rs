853d7fb127e6aaec924fca400864acd1a203bc2b
grant <ruddickmg@gmail.com>
2024-09-28 10:44:42 -1000

    feat: update version

--- src/utilities/logging.rs
+++ src/utilities/logging.rs
@@ -6,13 +6,13 @@ pub fn initialize() {
     let config = LoggingConfiguration::new();
     let directive = if config.level < tracing::Level::DEBUG {
         [&env!("CARGO_PKG_NAME").replace("-", "_"), "lib", "server"]
-          .map(| target | format!("{}={}", target, config.level))
-          .join(",")
+            .map(|target| format!("{}={}", target, config.level))
+            .join(",")
     } else {
         config.level.to_string()
     };
-    let subscriber = tracing_subscriber::FmtSubscriber::builder()
-      .with_env_filter(EnvFilter::new(&directive));
+    let subscriber =
+        tracing_subscriber::FmtSubscriber::builder().with_env_filter(EnvFilter::new(&directive));
 
     match config.format.as_str() {
         "json" => subscriber.json().init(),
