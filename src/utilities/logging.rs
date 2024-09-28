dce17eeb9fede64e2fc28e3637ef9c6051ce5437
grant <ruddickmg@gmail.com>
2024-09-28 10:32:48 -1000

    feat: show detailed logs for debug or above level

--- src/utilities/logging.rs
+++ src/utilities/logging.rs
@@ -4,12 +4,15 @@ use tracing_subscriber::EnvFilter;
 
 pub fn initialize() {
     let config = LoggingConfiguration::new();
-    let directive = [&env!("CARGO_PKG_NAME").replace("-", "_"), "lib", "server"]
-      .map(| target | format!("{}={}", target, config.level))
-      .join(",");
-    let filter = EnvFilter::new(&directive);
+    let directive = if config.level < tracing::Level::DEBUG {
+        [&env!("CARGO_PKG_NAME").replace("-", "_"), "lib", "server"]
+          .map(| target | format!("{}={}", target, config.level))
+          .join(",")
+    } else {
+        config.level.to_string()
+    };
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
-      .with_env_filter(filter);
+      .with_env_filter(EnvFilter::new(&directive));
 
     match config.format.as_str() {
         "json" => subscriber.json().init(),
