ed70f9b5cd8f4b9b4904ad6f05fa179e26a0188e
grant <ruddickmg@gmail.com>
2024-09-29 20:33:43 -1000

    feat: update formatting

--- src/configuration/authentication/kubernetes.rs
+++ src/configuration/authentication/kubernetes.rs
@@ -2,15 +2,15 @@ const DEFAULT_KUBERNETES_AUTH_PATH: &str = "kubernetes";
 
 #[derive(Clone, Debug)]
 pub struct Kubernetes {
-  pub file_path: String,
-  pub mount_path: String,
+    pub file_path: String,
+    pub mount_path: String,
 }
 
 impl Kubernetes {
-  pub fn new(file_path: String, mount_path: Option<String>) -> Self {
-    Self {
-      file_path,
-      mount_path: mount_path.unwrap_or(DEFAULT_KUBERNETES_AUTH_PATH.to_string()),
+    pub fn new(file_path: String, mount_path: Option<String>) -> Self {
+        Self {
+            file_path,
+            mount_path: mount_path.unwrap_or(DEFAULT_KUBERNETES_AUTH_PATH.to_string()),
+        }
     }
-  }
 }
--- src/configuration/authentication/user_pass.rs
+++ src/configuration/authentication/user_pass.rs
@@ -2,17 +2,17 @@ const DEFAULT_USER_PASS_AUTH_PATH: &str = "userpass";
 
 #[derive(Clone, Debug)]
 pub struct UserPass {
-  pub username: String,
-  pub password: String,
-  pub mount_path: String,
+    pub username: String,
+    pub password: String,
+    pub mount_path: String,
 }
 
 impl UserPass {
-  pub fn new(username: String, password: String, mount_path: Option<String>) -> Self {
-    Self {
-      username,
-      password,
-      mount_path: mount_path.unwrap_or(DEFAULT_USER_PASS_AUTH_PATH.to_string()),
+    pub fn new(username: String, password: String, mount_path: Option<String>) -> Self {
+        Self {
+            username,
+            password,
+            mount_path: mount_path.unwrap_or(DEFAULT_USER_PASS_AUTH_PATH.to_string()),
+        }
     }
-  }
 }
