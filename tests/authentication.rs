5e4e0266b6de6b967a718259df1f4d5b25d47900
grant <ruddickmg@gmail.com>
2024-09-30 21:19:56 -1000

    feat: add test for app role auth

--- tests/authentication.rs
+++ tests/authentication.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod authentication {
-    use lib::configuration::authentication::{Credentials, UserPass};
+    use lib::configuration::authentication::{AppRole, Credentials, UserPass};
     use lib::configuration::vault::VaultConfiguration;
     use lib::vault::Client;
     use vaultrs::client::{VaultClient, VaultClientSettingsBuilder};
@@ -8,15 +8,39 @@ mod authentication {
     #[tokio::test]
     async fn login_with_username_and_password() {
         let config: VaultConfiguration = VaultConfiguration {
+            role: "vault-kms-provider".to_string(),
+            address: "https://127.0.0.1:8400".to_string(),
+            transit_key: "vault-kms-provider".to_string(),
+            mount_path: "transit".to_string(),
             credentials: Credentials::UserPass(UserPass {
                 username: "vault-kms-provider".to_string(),
                 password: "password".to_string(),
                 mount_path: "userpass".to_string(),
             }),
+        };
+        let settings = VaultClientSettingsBuilder::default()
+            .address(&config.address)
+            .ca_certs(vec!["./test_files/certs/ca.crt".to_string()])
+            .build()
+            .unwrap();
+        let vault_client = VaultClient::new(settings).unwrap();
+        let client = Client::new(vault_client, &config);
+        let result = client.get_token().await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn login_with_app_role() {
+        let config: VaultConfiguration = VaultConfiguration {
             role: "vault-kms-provider".to_string(),
             address: "https://127.0.0.1:8400".to_string(),
             transit_key: "vault-kms-provider".to_string(),
             mount_path: "transit".to_string(),
+            credentials: Credentials::AppRole(AppRole::new(
+                "role".to_string(),
+                "secret".to_string(),
+                None,
+            )),
         };
         let settings = VaultClientSettingsBuilder::default()
             .address(&config.address)
