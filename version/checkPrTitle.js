bf3d97a157701b0e599f9ea8879b35f98741f2f6
grant <ruddickmg@gmail.com>
2024-10-05 22:13:51 -1000

    feat: checkout script

--- version/checkPrTitle.js
+++ version/checkPrTitle.js
@@ -3,6 +3,6 @@
   const types = ['chore', 'patch', 'fix', 'feat'];
   const [commitType] = message?.split(/[^A-Za-z]/i) || [];
   if (!types.includes(commitType) && !message?.toLowerCase().includes('breaking change')) {
-    throw new Error(`Invalid commit name, must include "BREAKING CHANGE" or one of the following prefixes: ${types.map(s => `"${s}"`).join(',')}`);
+    throw new Error(`Invalid PR title. Title must include "BREAKING CHANGE" or one of the following prefixes: ${types.map(s => `"${s}"`).join(',')}`);
   }
 })();
