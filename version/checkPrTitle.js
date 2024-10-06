e0343462674b530683b2f9f7c8ea9ab1a2a9b02e
grant <ruddickmg@gmail.com>
2024-10-05 22:48:31 -1000

    feat: update check

--- version/checkPrTitle.js
+++ version/checkPrTitle.js
@@ -1,9 +1,8 @@
 (() => {
   const [_node, _file, message] = process.argv;
-  console.log("message", message);
   const types = ['chore', 'patch', 'fix', 'feat'];
-  const [commitType] = message?.split(/[^A-Za-z]/i) || [];
+  const [commitType] = message?.trim().split(/[^A-Za-z]/i) || [];
   if (!types.includes(commitType) && !message?.toLowerCase().includes('breaking change')) {
-    // throw new Error(`Invalid PR title. Title must include "BREAKING CHANGE" or one of the following prefixes: ${types.map(s => `"${s}"`).join(',')}`);
+    throw new Error(`Invalid PR title. Title must include "BREAKING CHANGE" or one of the following prefixes: ${types.map(s => `"${s}"`).join(',')}`);
   }
 })();
