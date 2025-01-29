888d9d8a984982b34e34cadf97d3d9e5207c3f07
grant <ruddickmg@gmail.com>
2025-01-28 19:10:41 -1000

    chore: correct version string

--- src/vault/service.rs
+++ src/vault/service.rs
@@ -12,6 +12,7 @@ use tonic::{Code, Request, Response, Status};
 use tracing::{debug, error, info, instrument};
 
 const OKAY_RESPONSE: &str = "ok";
+const API_VERSION: &str = "v2";
 
 pub struct VaultKmsServer {
     client: Arc<RwLock<client::Client>>,
@@ -52,11 +53,13 @@ impl KeyManagementService for VaultKmsServer {
         debug!("Status request");
         let client = self.client.read().await;
         Ok(client.request_key().await.map(|key| {
-            Response::new(StatusResponse {
-                version: key.version,
+            let response = Response::new(StatusResponse {
+                version: API_VERSION.to_string(),
                 key_id: key.id,
                 healthz: OKAY_RESPONSE.to_string(),
-            })
+            });
+            debug!("Status response: {:?}", response);
+            response
         })?)
     }
 
